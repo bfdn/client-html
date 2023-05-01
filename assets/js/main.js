43a0bbae58a1c5fd72ecbc9c578bba7bbcbdf012
Bilal Fidan <bilalfiddan@gmail.com>
2023-05-01 11:25:56 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    nav: true,
+    loop: false,
+    items: 1
+    //margin: 10,
+    /*responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 1
+        },
+    }*/
+});
